fd39cba6ca6e9907ae1313e0a8642bc14a3cdc24
Ommmmmmmmmmm <kecb5533@yahoo.com.tw>
2017-01-07 23:39:11 +0800

    修掉不要的註解

--- webpage/index.php
+++ webpage/index.php
@@ -1,26 +1,26 @@
 <?php
+require_once("function/ession_manager.php");
+
 
 if($_COOKIE['stage']){
 	switch($_COOKIE['stage']){
-		case 'waiting': 
-		/** 等待中 **/
-			require_once("wait.php");
-			HTML("wait.html");
-			WAITING();
+		case 'waiting': /** 等待中 **/
+			HTML("module/wait.html");
 			break;
-		case 'playing':
-		/** 遊戲中 **/
-
+		case 'playing': /** 遊戲中 **/
+		
+			HTML("module/play_game.html");
 			break;
-
+		case 'ending': /** 遊戲結束 **/
+			HTML("module/end_game.html");
+			break;
 	}
 }
 else{ 
 /** 創建房間 **/
-	HTML("game_create.html");
+	HTML("module/game_create.html");
 }
-function HTML($target){
-/** 顯示網頁 **/
+function HTML($target){ /** 顯示網頁 **/
 	if(is_file($target))
 		readfile($target);
 }
