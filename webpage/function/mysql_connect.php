2b2f4a6a937fb6855cfb5d214742be06e0bda53f
garyuu <shuhanrpg@gmail.com>
2017-01-07 20:41:32 +0800

    Complete mysql connection functions

--- webpage/function/mysql_connect.php
+++ webpage/function/mysql_connect.php
@@ -1,3 +1,25 @@
 <?php
+require_once('mysql_connection_setting.php')
+
+class MySQLConnect{
+    private $host; 
+    private $user;
+    private $password;
+    private $database;
+    private $dbh;
+
+    function __construct(){
+        $this->host = DB_HOST;
+        $this->user = DB_USER;
+        $this->password = DB_PASSWORD;
+        $this->database = DB_NAME;
+        $this->dbh = sql_connect;
+    }
+
+    function sql_connect(){
+        $dsn = 'mysql:host='.$this->host.';dbname='.$this->database;
+        return new PDO($dsn, $this->user, $this->password);
+    }
     
+}    
 ?>
